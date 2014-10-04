14a1663b5877ed95bb3b4eac48c393b7a05fe203
Eduardo Alvarado Díaz <GLB-MXM0001@Eduardos-MacBook-Pro-2.local>
2014-10-03 23:51:36 -0500

    Initial Commit

---
+++ BlogReader/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  BlogReader
+//
+//  Created by Eduardo Alvarado Díaz on 10/3/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ BlogReader/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  BlogReader
+//
+//  Created by Eduardo Alvarado Díaz on 10/3/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
