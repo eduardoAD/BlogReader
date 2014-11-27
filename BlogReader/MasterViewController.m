d65d71d1369c49749a5fca5aa11877d5fdbee40c
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-11-27 17:32:12 -0600

    Get post data from url

--- BlogReader/MasterViewController.m
+++ BlogReader/MasterViewController.m
@@ -9,10 +9,6 @@
 #import "MasterViewController.h"
 #import "DetailViewController.h"
 
-//@interface MasterViewController ()
-//
-//@property NSMutableArray *objects;
-//@end
 
 @implementation MasterViewController
 
@@ -26,15 +22,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    // Do any additional setup after loading the view, typically from a nib.
-//    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-//
-//    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-//    self.navigationItem.rightBarButtonItem = addButton;
-    self.titlesArray = [NSArray arrayWithObjects:@"Title 1", @"Title 2", @"Title 3", @"Title 4", nil];
-    NSArray *booksArray = [NSArray arrayWithObjects:@"Hamlet",@"King Lear",@"Othello",@"Macbeth",nil];
-    NSString *bookTitle = booksArray[2];
-    [bookTitle uppercaseString];
+
+    NSURL *blogURL = [NSURL URLWithString:@"http://blog.teamtreehouse.com/api/get_recent_summary/"];
+
+    NSData *jsonData = [NSData dataWithContentsOfURL:blogURL];
+
+    NSError *error = nil;
+    NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+
+
+    self.blogPosts = [dataDictionary objectForKey:@"posts"];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,21 +40,13 @@
     // Dispose of any resources that can be recreated.
 }
 
-//- (void)insertNewObject:(id)sender {
-//    if (!self.objects) {
-//        self.objects = [[NSMutableArray alloc] init];
-//    }
-//    [self.objects insertObject:[NSDate date] atIndex:0];
-//    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-//    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-//}
-
 #pragma mark - Segues
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSString *title = self.titlesArray[indexPath.row];
+        NSDictionary *blogPost = self.blogPosts[indexPath.row];
+        NSString *title = [blogPost valueForKey:@"title"];
         [[segue destinationViewController] setDetailItem:title];
     }
 }
@@ -68,21 +58,23 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.titlesArray.count;
+    return self.blogPosts.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSString *object = self.titlesArray[indexPath.row];
-    cell.textLabel.text = object;
+    NSDictionary *blogPost = self.blogPosts[indexPath.row];
+    cell.textLabel.text = [blogPost valueForKey:@"title"];
+    cell.detailTextLabel.text = [blogPost valueForKey:@"author"];
     return cell;
 }
 
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     //[self performSegueWithIdentifier:@"showDetail" sender:self];
-    NSString *title = self.titlesArray[indexPath.row];
+    NSDictionary *blogPost = self.blogPosts[indexPath.row];
+    NSString *title = [blogPost valueForKey:@"title"];
 
     UIStoryboard *goDetail = [UIStoryboard storyboardWithName:@"MyStoryboard" bundle:nil];
     UIViewController *initialViewController = [goDetail instantiateInitialViewController];
@@ -94,18 +86,5 @@
     }];
 }
 
-//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-//    // Return NO if you do not want the specified item to be editable.
-//    return YES;
-//}
-//
-//- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-//    if (editingStyle == UITableViewCellEditingStyleDelete) {
-//        [self.objects removeObjectAtIndex:indexPath.row];
-//        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-//    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-//        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-//    }
-//}
 
 @end
